db180f8e97b9e192552c18599757d65590ea2a1f
Geovanny N Silva <geovanny.gns1@gmail.com>
2025-09-01 15:25:10 -0300

    add: nível aventureiro

---
+++ README.md
@@ -0,0 +1,41 @@
+Carta 1:
+
+Estado: A
+
+Código: A01
+
+Nome da Cidade: São Paulo
+
+População: 12325000
+
+Área: 1521.11 km²
+
+PIB: 699.28 bilhões de reais
+
+Número de Pontos Turísticos: 50
+
+Densidade Populacional: 8102.47 hab/km²
+
+PIB per Capita: 56724.32 reais
+
+
+
+Carta 2:
+
+Estado: B
+
+Código: B02
+
+Nome da Cidade: Rio de Janeiro
+
+População: 6748000
+
+Área: 1200.25 km²
+
+PIB: 300.50 bilhões de reais
+
+Número de Pontos Turísticos: 30
+
+Densidade Populacional: 5622.24 hab/km²
+
+PIB per Capita: 44532.91 reais
--- super_trunfo.c
+++ super_trunfo.c
@@ -2,7 +2,6 @@
 #include <string.h>
 
 int main() {
-    int qtdCartas;
     char estado[2];
     char codCarta[3];
     char cidade[50];
@@ -10,11 +9,11 @@ int main() {
     float area;
     float pib;
     int qtdPontosTuristicos;
+    float densidadepopulacional;
+    float pibPerCapita;
 
-    printf("Quantas cartas irá adicionar?");
-    scanf("%d", &qtdCartas);
+        // Carta 1
 
-    for (int i = 0; i< qtdCartas; i++) {
         printf("Digite o estado: ");
         scanf("%s", estado);
 
@@ -40,15 +39,60 @@ int main() {
 
         printf("\n\n---------------------------------\n\n");
 
-        printf("Carta %d:\n",i+1);
+        printf("Carta 1:\n");
         printf("Estado: %s\n", estado);
         printf("Código: %s%s\n", estado, codCarta);
         printf("Nome da Cidade: %s\n", cidade);
         printf("População: %d\n", populacao);
-        printf("Área: %f km2\n", area);
-        printf("PIB: %f bilhões de reais\n",pib);
-        printf("Número de Pontos Turísticos: %d", qtdPontosTuristicos);
+        printf("Área: %.2f km²\n", area);
+        printf("PIB: %.2f bilhões de reais\n",pib);
+        printf("Número de Pontos Turísticos: %d\n", qtdPontosTuristicos);
+
+        // Nível aventureiro
+        printf("Densidade Populacional: %.2f hab/km²\n", densidadepopulacional = populacao / area);
+        printf("PIB per Capita: %.2f reais\n", pibPerCapita = (pib * 1000000000) / populacao);
+
+        printf("\n\n---------------------------------\n\n");
+
+        // Carta 2
+
+        printf("Digite o estado: ");
+        scanf("%s", estado);
+
+        printf("Digite o Código: ");
+        scanf("%s", codCarta);
+
+        getchar();
+        printf("Digite a Cidade: ");
+        fgets(cidade, sizeof(cidade), stdin);
+        cidade[strcspn(cidade, "\n")] = '\0';
+
+        printf("Digite a população: ");
+        scanf("%d", &populacao);
+
+        printf("Digite a área: ");
+        scanf("%f", &area);
+
+        printf("Digite o PIB: ");
+        scanf("%f", &pib);
+
+        printf("Digite a quantidade de pontos turísticos: ");
+        scanf("%d", &qtdPontosTuristicos);
+
+        printf("\n\n---------------------------------\n\n");
+
+        printf("Carta 2:\n");
+        printf("Estado: %s\n", estado);
+        printf("Código: %s%s\n", estado, codCarta);
+        printf("Nome da Cidade: %s\n", cidade);
+        printf("População: %d\n", populacao);
+        printf("Área: %.2f km²\n", area);
+        printf("PIB: %.2f bilhões de reais\n",pib);
+        printf("Número de Pontos Turísticos: %d\n", qtdPontosTuristicos);
+
+        // Nível aventureiro
+        printf("Densidade Populacional: %.2f hab/km²\n", densidadepopulacional = populacao / area);
+        printf("PIB per Capita: %.2f reais\n", pibPerCapita = (pib * 1000000000) / populacao);
 
         printf("\n\n---------------------------------\n\n");
     }
-}
